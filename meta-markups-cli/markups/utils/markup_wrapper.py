e165d7111a09cbf25dc776f58dc860cad9aa2623
Shpana <romankozellaga@gmail.com>
2021-08-28 16:28:50 +0300

    Подумал и убрал пример с cli

--- meta-markups-cli/markups/utils/markup_wrapper.py
+++
@@ -1,7 +0,0 @@
-from ..markup_context import IMarkupContext, TMarkupOption, TTarget
-
-
-def markup_wrapper(
-        context: IMarkupContext, target: TTarget, option: TMarkupOption) -> TTarget:
-    context.add_option(target, option)
-    return target
