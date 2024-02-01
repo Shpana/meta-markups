b8bcba0941638ef23e7a675942369806b20e162f
Shpana <romankozellaga@gmail.com>
2024-02-01 18:58:55 +0300

    refactor: Исправил забавный баг: целью RuntimeMarkupContext должны быть только Hasheble сущности

--- meta-markups/examples/field_parsing.py
+++ meta-markups/examples/field_parsing.py
@@ -1,7 +1,7 @@
 from typing import Callable, TypeVar
 
 from ..markups import (
-        wrap_attachment, RuntimeMarkupContext, TTarget
+        wrap_attachment, RuntimeMarkupContext, TRealTarget
     )
 
 
@@ -12,32 +12,42 @@ class ShowInConsoleAttribute:
     pass
 
 
-def show_in_console(target: TTarget) -> Callable:
+def show_in_console(target: TRealTarget) -> Callable:
     return wrap_attachment(
         context, target, ShowInConsoleAttribute())
 
 
-def should_show_in_console(target: TTarget) -> bool:
+def should_show_in_console(target: TRealTarget) -> bool:
     return len(list(
         context.try_get_attributes_with_type(target, ShowInConsoleAttribute))) > 0
 
 
 class Player:
     
+    __health: int
+
     @show_in_console
     @property
-    def health(self) -> float: ...
+    def health(self) -> float: 
+        return self.__health
 
+    @show_in_console
     @property
-    def max_possible_health(self) -> float: ...
+    def max_possible_health(self) -> float: 
+        return 100
+
+    def __init__(self) -> None:
+        self.__health = self.max_possible_health - 20
 
 
 TComponent = TypeVar("TComponent")
 
+
 def show_fields_in_console(component: TComponent) -> None: 
     for key, value in type(component).__dict__.items():
-        if should_show_in_console(value):
-            print(key)
+        if isinstance(value, TRealTarget.__bound__):
+            if should_show_in_console(value):
+                print(f"- {key}={value.fget(component)}")
 
 
 if __name__ == "__main__":
